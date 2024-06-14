fc683a603115dcfa036e8d6778ecde23558c0a5f
lqqyt2423 <974923609@qq.com>
2024-06-14 14:23:38 +0800

    eval prefix

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -14,20 +14,59 @@ var (
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
-		var result object.Object
-		for _, stmt := range node.Statements {
-			result = Eval(stmt)
-		}
-		return result
+		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	default:
 		return nil
 	}
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+	return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
 
 func nativeBoolToBooleanObject(b bool) *object.Boolean {
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -15,6 +15,8 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
 	}
 	for _, tt := range tests {
 		tt := tt
@@ -41,6 +43,36 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.input, func(t *testing.T) {
+			l := lexer.New(tt.input)
+			p := parser.New(l)
+			program := p.ParseProgram()
+			obj := Eval(program)
+			bobj, ok := obj.(*object.Boolean)
+			if !ok {
+				t.Fatalf("should be *object.Boolean, but got %T", obj)
+			}
+			if bobj.Value != tt.wantValue {
+				t.Fatalf("value want %t, but got %t", tt.wantValue, bobj.Value)
+			}
+		})
+	}
+}
+
+func TestBandOperator(t *testing.T) {
+	tests := []struct {
+		input     string
+		wantValue bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
 	}
 	for _, tt := range tests {
 		tt := tt
